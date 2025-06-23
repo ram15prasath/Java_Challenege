de8bd18bb93989a32d4b9632514bfb2871beddb7
Ramprasath <ca235113646@bhc.edu.in>
2025-06-23 20:22:32 +0530

    Add files via upload
    Java Challenge3

---
+++ Operators.java
@@ -0,0 +1,28 @@
+public class Operators{
+    public static void main(String[] args) {
+        
+        int a=5;
+        int b=10;
+        //Arithemetic Operations
+        int c=a+b;
+        int d=a-b;
+        int e=a*b;
+        int g=a%b;
+        
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        //Type Casting
+        float f=(float)a/(float)b;
+        System.out.println(f);
+        System.out.println(g);
+
+        //Unary Opeartor
+        a++;
+        System.out.println(a);
+        a--;
+        System.out.println(a);
+
+
+    }
+}
